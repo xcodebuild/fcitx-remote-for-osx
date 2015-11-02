b48b0f7f309402f2bfe394de2b6626b699213582
codefalling <code.falling@gmail.com>
2015-11-02 16:21:35 +0800

    update readme for vim

--- fcitx-remote/main.m
+++ fcitx-remote/main.m
@@ -9,11 +9,30 @@
 #import <Foundation/Foundation.h>
 #import <Carbon/Carbon.h>
 
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <limits.h>
+
 #define INACTIVE 1
 #define ACTIVE 2
 
 #define INACTIVE_STR "1"
 #define ACTIVE_STR "2"
+
+#define CMD_GET_STATUS 0
+#define CMD_INACTIVATE_ACTIVATE 1
+#define CMD_SWITCH_BETWEEN 3
+#define CMD_SWITCH_TO 4
 
 #ifndef US_KEYBOARD_LAYOUT
     #define US_KEYBOARD_LAYOUT @"com.apple.keylayout.US"
@@ -22,6 +41,27 @@
 #ifndef CHINNESE_KEYBOARD_LAYOUT
     #define CHINNESE_KEYBOARD_LAYOUT @"com.sogou.inputmethod.sogou.pinyin"
 #endif
+
+
+int create_socket(const char *name)
+{
+    int fd;
+    int r;
+    struct sockaddr_un uds_addr;
+    fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0) {
+        return fd;
+    }
+    /* setup address struct */
+    memset(&uds_addr, 0, sizeof(uds_addr));
+    uds_addr.sun_family = AF_UNIX;
+    strcpy(uds_addr.sun_path, name);
+    r = connect(fd, (struct sockaddr *) & uds_addr, sizeof(uds_addr));
+    if (r < 0) {
+        return r;
+    }
+    return fd;
+}
 
 
 void switch_to(NSString* imId){
@@ -86,16 +126,32 @@ void print_help(){
 
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
+        char *socketfile = NULL;
+        int socket_fd;
+
+        asprintf(&socketfile, "/tmp/fcitx-socket-:%d", 1);
+        socket_fd = create_socket(socketfile);
+        if (socket_fd < 0) {
+            fprintf(stderr, "Can't open socket %s: %s\n", socketfile, strerror(errno));
+            free(socketfile);
+            return 1;
+        }
+        free(socketfile);
+
         if (argc > 1) {
             NSString *method = [NSString stringWithUTF8String:argv[1]];
             if([method isEqualToString:@"-o"]){
                 active();
+
             }else if([method isEqualToString:@"-c"]){
                 inactive();
+
             }else if([method isEqualToString:@"-t"]){
                 switch_between_active_inactive();
+
             }else if([method isEqualToString:@"-s"]){
                 switch_to([NSString stringWithUTF8String:argv[2]]);
+
             }else if([method isEqualToString:@"-h"]){
                 print_help();
             }else if([method isEqualToString:@"-n"]){
@@ -105,7 +161,11 @@ int main(int argc, const char *argv[]) {
             print_status(get_status());
         }
 
+
+
+        return 0;
+
     }
 
-    return 0;
+
 }
